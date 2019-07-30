fd68bdb5df1212dc9a23696390bba1fd50a6e780
Lautaro Ivan Gonzalez <glautaroivan@gmail.com>
2019-07-29 22:18:29 -0300

    added styled components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import H2Element from '../styled/H2Element';
+
+const SkillsContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 2rem;
+    grid-row-gap: 0.5em;
+`;
+
+const SkillsH5 = styled.h5`
+  margin: .5em 0;
+`;
+
+const SkillsLine = styled.div`
+  height: 8px;
+  position: relative;
+  border-radius: 50px;
+  background: #F8BBD0;
+`;
+
 
 const Skills = props => (
     <div className="Skills">
-        <div className="Skills-container">
+        <H2Element name="Skills"/>
+        <SkillsContainer>
             {props.data.map( ( skill, index ) => (
             <div className="Skills-item" key={`Skills-${index}`}>
-                <h5>{skill.name}</h5>
-                <div className="Skills-line">
+                <SkillsH5>{skill.name}</SkillsH5>
+                <SkillsLine>
                     <span>{skill.percentage}</span>
-                </div>
+                </SkillsLine>
             </div>
             ))}
-        </div>
+        </SkillsContainer>
     </div>
 );
 
